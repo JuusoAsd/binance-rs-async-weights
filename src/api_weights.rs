89a3fce5b80e8aecace4674efb8c38c1027a9ae8
JuusoAsd <jahlroos96@gmail.com>
2024-03-05 16:45:25 +0200

    get weights to file

--- src/api_weights.rs
+++ src/api_weights.rs
@@ -4,8 +4,9 @@ use crate::market::Market;
 
 use futures::Future;
 use log::info;
-use std::fmt::Debug;
+use std::io::Write;
 use std::sync::Mutex;
+use std::{fmt::Debug, fs::OpenOptions};
 
 use log::{Level, LevelFilter, Log, Metadata, Record};
 use redis::Commands;
@@ -71,18 +72,18 @@ pub async fn setup_rate_limiter() {
     rate_limiter_setup_futures().await;
 }
 
-struct CustomLogHandler {
+struct CustomLogHandlerRedis {
     current_usage_spot: Mutex<i32>,
     current_usage_fut: Mutex<i32>,
     prev_key: Mutex<String>,
     db_client: Mutex<redis::Connection>,
 }
-impl CustomLogHandler {
-    fn new() -> CustomLogHandler {
+impl CustomLogHandlerRedis {
+    fn new() -> CustomLogHandlerRedis {
         let client = redis::Client::open("redis://127.0.0.1/").unwrap();
         let con = client.get_connection().unwrap();
 
-        CustomLogHandler {
+        CustomLogHandlerRedis {
             current_usage_spot: Mutex::new(0),
             current_usage_fut: Mutex::new(0),
             prev_key: Mutex::new("empty".to_string()),
@@ -137,7 +138,102 @@ impl CustomLogHandler {
         }
     }
 }
-impl Log for CustomLogHandler {
+impl Log for CustomLogHandlerRedis {
+    fn enabled(&self, metadata: &Metadata) -> bool {
+        metadata.level() <= Level::Info // Adjust this based on what levels you want to capture
+    }
+
+    fn log(&self, record: &Record) {
+        if self.enabled(record.metadata()) {
+            let val: Result<Value, serde_json::Error> =
+                serde_json::from_str(record.clone().args().to_string().as_str());
+            match val {
+                Ok(v) => {
+                    self.process_log(v);
+                }
+                Err(_) => {
+                    let mut prev_val = self.prev_key.lock().unwrap();
+                    *prev_val = record.args().to_string();
+                }
+            }
+        }
+    }
+
+    fn flush(&self) {}
+}
+
+struct CustomLogHandlerFile {
+    current_usage_spot: Mutex<i32>,
+    current_usage_fut: Mutex<i32>,
+    prev_key: Mutex<String>,
+    file_path: String,
+}
+
+impl CustomLogHandlerFile {
+    fn new() -> CustomLogHandlerFile {
+        CustomLogHandlerFile {
+            current_usage_spot: Mutex::new(0),
+            current_usage_fut: Mutex::new(0),
+            prev_key: Mutex::new("empty".to_string()),
+            file_path: "api_weights.txt".to_string(),
+        }
+    }
+    fn update_weight(&self, market_type: &str, new_weight: i32) {
+        // Determine the correct current_usage based on market_type
+        let current_usage = match market_type {
+            "spot" => &self.current_usage_spot,
+            _ => &self.current_usage_fut,
+        };
+
+        let old_weight = *current_usage.lock().expect("Lock current usage");
+        let request_weight: i32;
+        if new_weight < old_weight {
+            request_weight = new_weight;
+            println!("{} - {}", *self.prev_key.lock().expect("Lock prev_key"), new_weight);
+        } else {
+            request_weight = new_weight - old_weight;
+            println!(
+                "{} - {}",
+                *self.prev_key.lock().expect("Lock prev_key"),
+                new_weight - old_weight
+            );
+        }
+
+        let key = self.prev_key.lock().expect("Lock prev_key").clone();
+        let log_entry = format!("{}: {}\n", key, request_weight);
+        // Update Redis and the current usage
+        let mut file = OpenOptions::new()
+            .create(true)
+            .append(true)
+            .open(&self.file_path)
+            .expect("Unable to open file");
+
+        file.write_all(log_entry.as_bytes()).expect("Unable to write data");
+
+        // Update the current usage
+        *current_usage.lock().expect("Lock current usage") = new_weight;
+        *current_usage.lock().expect("Lock current usage") = new_weight;
+    }
+
+    fn process_log(&self, v: Value) {
+        if let Some(new_weight_str) = v
+            .get("x-mbx-used-weight")
+            .and_then(|v| v.as_str())
+            .or_else(|| v.get("x-mbx-used-weight-1m").and_then(|v| v.as_str()))
+        {
+            if let Ok(new_weight) = new_weight_str.parse::<i32>() {
+                let market_type = if self.prev_key.lock().expect("Lock prev_key").starts_with("spot") {
+                    "spot"
+                } else {
+                    "futures"
+                };
+                self.update_weight(market_type, new_weight);
+            }
+        }
+    }
+}
+
+impl Log for CustomLogHandlerFile {
     fn enabled(&self, metadata: &Metadata) -> bool {
         metadata.level() <= Level::Info // Adjust this based on what levels you want to capture
     }
@@ -162,7 +258,15 @@ impl Log for CustomLogHandler {
 }
 
 pub async fn setup_to_db() {
-    log::set_boxed_logger(Box::new(CustomLogHandler::new()))
+    log::set_boxed_logger(Box::new(CustomLogHandlerRedis::new()))
+        .map(|()| log::set_max_level(LevelFilter::Info))
+        .unwrap(); // Adjust level as needed
+
+    setup_rate_limiter().await;
+}
+
+pub async fn setup_to_file() {
+    log::set_boxed_logger(Box::new(CustomLogHandlerFile::new()))
         .map(|()| log::set_max_level(LevelFilter::Info))
         .unwrap(); // Adjust level as needed
 
